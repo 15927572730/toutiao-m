cda055f03350d54e8bea6b504d48ecb9ecf3d888
15927572730 <1158194011@qq.com>
2021-11-04 19:31:08 +0800

    头条项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//加载Vant核心组件库W
+import Vant from 'vant'
+
+//加载Vant全局样式
+import 'vant/lib/index.css'
+
 import './styles/index.less'
+
+import router from './router/index.js'
+
+//注册使用vant组件库
+Vue.use(Vant);
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
